fc6ef5dc7ff7df1ae97d19805e2257d0b715e2ef
Sreehari S <sreeharisree297@gmail.com>
2026-09-24 11:04:05 +0530

    Create Count_characters_in_a_string_with_and_without_built_in.py

---
+++ Count_characters_in_a_string_with_and_without_built_in.py
@@ -0,0 +1,8 @@
+#with built in
+n=input("Enter the String:")
+print("Length of the string(built in)=",len(n))
+#without built in
+count=0
+for i in n:
+  count=count+1
+print("Length of the string(without built in)=",count)
