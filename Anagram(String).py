f4b01beb372e45ba280b7835af6d1f93b9d30c2d
Sreehari S <sreeharisree297@gmail.com>
2026-09-24 10:58:23 +0530

    Update Anagram(String).py

--- Anagram(String).py
+++ Anagram(String).py
@@ -1,5 +1,5 @@
-str1=input("Enter First String")
-str2=input("Enter Second String")
+str1=input("Enter First String:")
+str2=input("Enter Second String:")
 if sorted(str1)==sorted(str2):
   print("The strings are Anagrams")
 else:
