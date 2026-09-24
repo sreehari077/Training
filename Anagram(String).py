9d9f019414d6a21ac2eb5bea385d8d8e59e010b1
Sreehari S <sreeharisree297@gmail.com>
2026-09-24 10:56:47 +0530

    Create Anagram(String).py

---
+++ Anagram(String).py
@@ -0,0 +1,6 @@
+str1=input("Enter First String")
+str2=input("Enter Second String")
+if sorted(str1)==sorted(str2):
+  print("The strings are Anagrams")
+else:
+    print("The strings are not Anagrams")
